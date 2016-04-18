988b929ceeb0ee3780cf2f90fbb6330ca83e139e
fsarradin@ippon.fr <fsarradin@gmail.com>
2016-04-18 22:29:05 +0200

    add base code for mixin

--- src/main/java/me/mixin/Main.java
+++ src/main/java/me/mixin/Main.java
@@ -6,4 +6,11 @@ import java.util.WeakHashMap;
 
 public class Main {
 
+    interface MyMixture extends Runnable {}
+
+    public static void main(String[] args) {
+        MyMixture mixture1 = () -> System.out.println("hello");
+        MyMixture mixture2 = () -> System.out.println("hello");
+    }
+
 }
